73dc680b215d6df0f0640d27bda2fbbde83fd0a5
Oscar Guio <opinzonguio@gmail.com>
2025-10-15 16:31:48 -0500

    Limpieza del repositorio: eliminar estructura vieja y mantener solo monteverde/

--- src/components/Card.jsx
+++
@@ -1,9 +0,0 @@
-export default function Card({ title, children, action, className = '' }) {
-  return (
-    <div className={`card ${className}`}>
-      {title && <div className="section-title">{title}</div>}
-      {children}
-      {action && <div style={{ marginTop: '.75rem' }}>{action}</div>}
-    </div>
-  )
-}
